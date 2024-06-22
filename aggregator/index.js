c1850b061ad01889bfb98ab209010632be1df06c
edenkim00 <eotjd0986@kaist.ac.kr>
2024-06-22 14:34:19 +0900

    feat: ranking

--- aggregator/index.js
+++ aggregator/index.js
@@ -1,3 +1,28 @@
-exports.handler = async function (event) {
-  console.log(event);
-};
+import {
+  selectAllActiveChallenges,
+  aggregateRankingForChallenge,
+  insertRankings,
+} from "./lib/db.js";
+import _ from "lodash";
+const KST_OFFSET = 9 * 60 * 60 * 1000;
+
+export async function handler() {
+  const dt = new Date(Date.now() + KST_OFFSET).toISOString().substring(0, 10);
+
+  const challenges = await selectAllActiveChallenges();
+  const challengeRankings = [];
+
+  // WARN: avoid using promise.
+  for (const challenge of challenges) {
+    challengeRankings.push(
+      await aggregateRankingForChallenge(challenge.id, dt)
+    );
+  }
+
+  const rankings = _.flatten(challengeRankings);
+  await insertRankings(rankings);
+  return {
+    statusCode: 200,
+    body: "Success",
+  };
+}
--- aggregator/jest.config.js
+++ aggregator/jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: {
     name: "aggregator",
     color: "green",
