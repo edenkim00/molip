af68ed38ed6b47efca83cefd593337a0e596bae5
edenkim00 <eotjd0986@kaist.ac.kr>
2024-06-22 16:16:18 +0900

    chore: image upload feature

--- backend/src/app/lib/image.js
+++ backend/src/app/lib/image.js
@@ -2,36 +2,34 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const s3Client = new S3Client({ region: "ap-northeast-2" });
 const { v4 } = require("uuid");
 const IMAGE_BUCKET_NAME = "molip-images";
-
+const { response, errResponse } = require("../../../config/response");
+const baseResponse = require("../../../config/baseResponseStatus");
 async function uploadImageToS3(data) {
-  const { image_file: file } = data;
-  console.log(file);
+  const { files } = data;
+  const file = files[0];
+  const contentType = file.contentType ?? getMimeType(file.filename);
 
-  // 파일의 mime 타입 확인
-  const contentType = file.mimetype || getMimeType(file);
-
-  // Public Read 권한을 가진 이미지 업로드
   const uploadParams = {
     Bucket: IMAGE_BUCKET_NAME,
     Key: `${v4().replace(/-/g, "").substring(0, 10)}_${
       file.name ?? file.originalname ?? ""
     }`,
-    Body: file,
+    Body: file.content,
     ContentType: contentType,
     ACL: "public-read",
   };
 
   try {
     const data = await s3Client.send(new PutObjectCommand(uploadParams));
-    return data.Location;
+    return response(baseResponse.SUCCESS, { imageUrl: data.Location });
   } catch (err) {
-    console.error("Error", err);
-    throw new Error("Failed to upload file: " + err.message);
+    console.error("[Image upload]", err);
+    return errResponse(baseResponse.DB_ERROR);
   }
 }
 
 function getMimeType(file) {
-  const extension = file.name.split(".").pop(); // 파일 확장자 추출
+  const extension = file.name?.split(".")?.pop(); // 파일 확장자 추출
   let mimeType = "";
 
   switch (extension) {
@@ -45,11 +43,9 @@ function getMimeType(file) {
     case "gif":
       mimeType = "image/gif";
       break;
-    // 다른 파일 확장자에 대한 mime 타입 추가
     default:
-      mimeType = "application/octet-stream"; // 기본적으로 binary 데이터로 처리
+      mimeType = "application/octet-stream";
   }
-
   return mimeType;
 }
 
