00e3058780749b17012bdca51211626e5d7c30c4
edenkim00 <eotjd0986@kaist.ac.kr>
2024-06-22 15:54:59 +0900

    chore: image upload feature

--- backend/src/app/lib/image.js
+++ backend/src/app/lib/image.js
@@ -6,6 +6,10 @@ const IMAGE_BUCKET_NAME = "molip-images";
 async function uploadImageToS3(data) {
   const { image_file: file } = data;
   console.log(Buffer.from(file, "binary"));
+
+  // 파일의 mime 타입 확인
+  const contentType = file.mimetype || getMimeType(file);
+
   // Public Read 권한을 가진 이미지 업로드
   const uploadParams = {
     Bucket: IMAGE_BUCKET_NAME,
@@ -13,7 +17,7 @@ async function uploadImageToS3(data) {
       file.originalname ?? ""
     }`,
     Body: Buffer.from(file, "binary"),
-    ContentType: file.mimetype ?? "image/jpg",
+    ContentType: contentType,
     ACL: "public-read",
   };
 
@@ -24,6 +28,29 @@ async function uploadImageToS3(data) {
     console.error("Error", err);
     throw new Error("Failed to upload file: " + err.message);
   }
+}
+
+function getMimeType(file) {
+  const extension = file.name.split(".").pop(); // 파일 확장자 추출
+  let mimeType = "";
+
+  switch (extension) {
+    case "jpg":
+    case "jpeg":
+      mimeType = "image/jpeg";
+      break;
+    case "png":
+      mimeType = "image/png";
+      break;
+    case "gif":
+      mimeType = "image/gif";
+      break;
+    // 다른 파일 확장자에 대한 mime 타입 추가
+    default:
+      mimeType = "application/octet-stream"; // 기본적으로 binary 데이터로 처리
+  }
+
+  return mimeType;
 }
 
 module.exports = {
