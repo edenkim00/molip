ebffd99442b41e10e86a4686c4f5b87584deba40
edenkim00 <eotjd0986@kaist.ac.kr>
2024-06-22 15:49:01 +0900

    chore: image upload feature

--- backend/src/app/lib/image.js
+++ backend/src/app/lib/image.js
@@ -1,17 +1,19 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const s3Client = new S3Client({ region: "ap-northease-2" });
+const s3Client = new S3Client({ region: "ap-northeast-2" });
 const { v4 } = require("uuid");
 const IMAGE_BUCKET_NAME = "molip-images";
 
 async function uploadImageToS3(data) {
   const { image_file: file } = data;
   console.log(Buffer.from(file, "binary"));
+
   const uploadParams = {
     Bucket: IMAGE_BUCKET_NAME,
     Key: `${v4().replace(/-/g, "").substring(0, 10)}_${
       file.originalname ?? ""
     }`,
-    Body: Buffer.from(file, "binary"), // file은 binary 데이터로 가정합니다.
+    Body: Buffer.from(file, "binary"),
+    ContentType: file.mimetype ?? "image/jpeg",
     ACL: "public-read",
   };
 
