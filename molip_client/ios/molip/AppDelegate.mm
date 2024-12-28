ef005e0cfc0c7ecd1dfee5a72553ddc9f8f0afa4
Ai Ke <aike@Aiui-MacBookPro.local>
2024-12-29 03:23:52 +0900

    chore: remove kakao login

--- molip_client/ios/molip/AppDelegate.mm
+++ molip_client/ios/molip/AppDelegate.mm
@@ -1,7 +1,6 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <RNKakaoLogins.h>
 
 @implementation AppDelegate
 
@@ -32,9 +31,7 @@
 - (BOOL)application:(UIApplication *)app
      openURL:(NSURL *)url
      options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
- if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
-    return [RNKakaoLogins handleOpenUrl: url];
- }
+ 
  return NO;
 }
 
